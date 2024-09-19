9978b2f1eb48eeff3b34efc4a5971b95d6d9ade3
Aman Patel <aman030904@gmail.com>
2024-09-20 00:39:06 +0530

    Modified the router functionality and css updated

--- src/crypto_ui/src/App.js
+++ src/crypto_ui/src/App.js
@@ -3,7 +3,17 @@ import { Container, Button, Typography, TextField, Paper, Box } from '@mui/mater
 import { ethers } from 'ethers';
 import TokenWatchList from './components/TokenWatchList';
 import TokenTransfer from './components/TokenTransfer';
+import TokenAllowance from './components/TokenAllowance';
+import Home from './components/Home';
+import Fetch from './components/Fetch';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
 import './App.css';
+
 
 function App() {
   const [walletAddress, setWalletAddress] = useState('');
@@ -31,18 +41,19 @@ function App() {
   };
 
   return (
-    <Container maxWidth="lg" className="container">
-      <Typography variant="h3" gutterBottom className="header">
+    <Container maxWidth="lg" className="container" >
+  
+    {/* <Typography variant="h3" gutterBottom className="header">
         Crypto Portfolio App
-      </Typography>
+      </Typography> */}
 
-      <Paper elevation={3} className="paper">
+    <Paper elevation={3} className="paper" sx={{ backgroundColor: '#34434e', color: '#f7f3f3ee' }}>
         <Typography variant="h5" gutterBottom align="center">
           Connect Your Wallet
         </Typography>
         <Box textAlign="center">
           {!manualInput ? (
-            <Button variant="contained" color="primary" onClick={connectWallet}>
+            <Button variant="contained" sx={{backgroundColor:'#f7f3f3ee',  color:'#000000'}}  onClick={connectWallet}>
               {walletAddress ? 'Connected: ' + walletAddress : 'Connect Wallet'}
             </Button>
           ) : (
@@ -60,28 +71,32 @@ function App() {
         </Box>
       </Paper>
 
-      {walletAddress && (
-        <Box className="main-content">
-          <Box className="content-item">
-            <Paper elevation={3} className="paper">
-              <Typography variant="h5" gutterBottom>
-                Token Watch List
-              </Typography>
-              <TokenWatchList walletAddress={walletAddress} provider={provider} />
-            </Paper>
-          </Box>
-          <Box className="content-item">
-            <Paper elevation={3} className="paper">
-              <Typography variant="h5" gutterBottom>
-                Token Transfer
-              </Typography>
-              <TokenTransfer walletAddress={walletAddress} provider={provider} />
-            </Paper>
-          </Box>
-        </Box>
+    
+
+      {  walletAddress && (
+        <Router>
+        <Navbar />
+        <Routes>
+            <Route exact path="/home" element={<Home />} />
+            <Route path="/allowance" element={<TokenAllowance />} />
+            <Route
+                path="/watch"
+                element={<TokenWatchList />}
+            />
+            <Route path="/transfer"
+              element={<TokenTransfer />}
+            />
+            <Route path="/history"
+              element={<Fetch />}
+            />
+        </Routes>
+    </Router>
       )}
-    </Container>
+
+</Container>
+    
   );
+  
 }
 
 export default App;
